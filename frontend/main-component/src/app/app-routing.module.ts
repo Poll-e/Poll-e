8411f0bd7c15057335cbc375b5e60a9872173858
dszondy <szondy.daniel@gmail.com>
2020-11-03 17:51:14 +0100

    Frontend folder structure created

--- frontend/main-component/src/app/app-routing.module.ts
+++ frontend/main-component/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {PollCreateComponent} from './components/poll-create/poll-create.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'polls/create', component: PollCreateComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/main-component/src/app/app.module.ts
+++ frontend/main-component/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { PollCreateComponent } from './components/poll-create/poll-create.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PollCreateComponent
   ],
   imports: [
     BrowserModule,
